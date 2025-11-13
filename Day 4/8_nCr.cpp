27149412b4963ce648d26cf15fe59118a358cd15
Roshan Gupta <roshangupta112113@gmail.com>
2025-11-13 22:22:43 +0530

    Add files via upload

---
+++ Day 4/8_nCr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fact(int x) {
+    int f = 1;
+    for (int i = 1; i <= x; i++)
+        f *= i;
+    return f;
+}
+
+int main() {
+    int n, r;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Enter r: ";
+    cin >> r;
+
+    int nCr = fact(n) / (fact(r) * fact(n - r));
+
+    cout << "nCr = " << nCr << endl;
+    return 0;
+}
